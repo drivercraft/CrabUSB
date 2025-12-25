87a0503c114aa630442176ef40a8be817f9bc306
周睿 <zrufo747@outlook.com>
2025-12-25 11:15:20 +0800

    feat(xhci): 添加设备上下文管理和事件环信息结构，重构主机控制器初始化逻辑

--- usb-host/src/backend/xhci/event.rs
+++ usb-host/src/backend/xhci/event.rs
@@ -62,4 +62,18 @@ impl EventRing {
     pub fn len(&self) -> usize {
         self.ste.len()
     }
+
+    pub fn info(&self) -> EventRingInfo {
+        EventRingInfo {
+            erstz: self.len() as _,
+            erdp: self.erdp(),
+            erstba: self.erstba(),
+        }
+    }
+}
+
+pub struct EventRingInfo {
+    pub erstz: u16,
+    pub erdp: u64,
+    pub erstba: u64,
 }
