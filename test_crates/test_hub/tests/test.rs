8ed883766d53cde608eb77403794b7e04fb2a106
周睿 <zrufo747@outlook.com>
2026-01-23 09:50:55 +0800

    fix typo in test file

--- test_crates/test_hub/tests/test.rs
+++ test_crates/test_hub/tests/test.rs
@@ -269,7 +269,7 @@ mod tests {
                     println!("irq: {irq:?}");
 
                     return Some(XhciInfo {
-                        usb: USBHost::new_xhci(addr, u32::MAX as usize).unwrap(),
+                        usb: USBHost::new_xhci(addr).unwrap(),
                         irq,
                     });
                 }
@@ -316,7 +316,7 @@ mod tests {
                 let irq = node.irq_info();
 
                 return XhciInfo {
-                    usb: USBHost::new_xhci(addr, u32::MAX as usize).unwrap(),
+                    usb: USBHost::new_xhci(addr).unwrap(),
                     irq,
                 };
             }
